60c7d9553117e82544b9ae48ca5a49488c024dd1
askhatkenenbay <askhat.kenenbay@nu.edu.kz>
2023-04-17 00:55:59 +0600

    commit

--- Leetcode/src/DetectPatternOfLengthMRepeatedKOrMoreTimes.java
+++ Leetcode/src/DetectPatternOfLengthMRepeatedKOrMoreTimes.java
@@ -5,6 +5,7 @@ public class DetectPatternOfLengthMRepeatedKOrMoreTimes {
     A pattern is a subarray (consecutive sub-sequence) that consists of one or more values, repeated multiple times consecutively without overlapping.
     A pattern is defined by its length and the number of repetitions.
     Return true if there exists a pattern of length m that is repeated k or more times, otherwise return false.
+    
      */
     public boolean containsPattern(int[] arr, int m, int k) {
         int count = 0;
